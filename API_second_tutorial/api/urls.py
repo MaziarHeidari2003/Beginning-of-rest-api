587c0b8f6369c42e0f5ed45f3daa47e1291a236b
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-05-01 10:51:29 -0700

    so two failed courses on rest_api, they suck! Lets head th third course

---
+++ API_second_tutorial/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path("blogposts/",views.BlogPostListCreate.as_view(), name='blogpost-view-create'),
+  path("blogposts/<int:pk>/",views.BlogPostRetrieveUpdateDestroy.as_view(), name='update'),
+  path("blogposts/<int:pk>/",views.BlogPostList.as_view(), name='update')
+
+
+]
---
+++ API_second_tutorial/mysite/db-script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sleep 10
+python manage.py makemigrations
+python manage.py migrate
