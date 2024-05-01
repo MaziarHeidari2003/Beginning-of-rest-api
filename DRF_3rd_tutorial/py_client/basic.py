06b462aeabf14b916a54a0dfadd33b90da7289e0
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-05-01 13:04:03 -0700

    done sending requests from client to django project

--- DRF_3rd_tutorial/py_client/basic.py
+++ DRF_3rd_tutorial/py_client/basic.py
@@ -1,10 +1,11 @@
 import requests
 
-endpoint = "http://httpbin.org/Status_codes/patch_status__codes_"
-endpoint = "http://httpbin.org/anything"
+#endpoint = "http://httpbin.org/Status_codes/patch_status__codes_"
+endpoint = "http://localhost:8000"   #"http://127.0.0.1:8000"
 
 get_response = requests.get(endpoint, json={"query":"hi bitch"})
 #print(get_response.text)
-print(get_response.json())
+#print(get_response.json())
 print(get_response.status_code)
+print(get_response.text)
 
