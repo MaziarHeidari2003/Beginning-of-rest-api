224e50fc19da3d9c6c52bdf2dc1fd91a4772c17a
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-05-03 11:14:43 -0700

    i just created serializers

--- DRF_3rd_tutorial/backend/api/views.py
+++ DRF_3rd_tutorial/backend/api/views.py
@@ -4,16 +4,30 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from products.serializers import ProductSerializer
 
 @api_view(['GET'])
 def api_home(request,*args,**kwargs):
  
-  model_data = Product.objects.all().order_by("?").first()
+  instance = Product.objects.all().order_by("?").first()
   data = {}
-  if model_data:
-    
-    data = model_to_dict(model_data, fields=['price'])
+  if instance:
+    data = ProductSerializer(instance).data
+    #data = model_to_dict(instance, fields=['title','sale_price','price'])
     #json_data_str = json.dumps(data)
   return Response(data)
   
   #return HttpResponse(json_data_str,headers={"content-type": "application/json"})
+"""
+ body = request.body   byte string of JSON data
+   data = {}
+   try:
+      data = json.loads(body)    RETURNS A DICT
+   except:
+      pass
+         
+   data['headers']= dict(request.headers)  -> not json serezible
+   data['content_type'] = request.content_type   
+  
+"""
+  
--- DRF_3rd_tutorial/py_client/basic.py
+++ DRF_3rd_tutorial/py_client/basic.py
@@ -1,13 +1,13 @@
 import requests
 
-#endpoint = "http://httpbin.org/Status_codes/patch_status__codes_"
+#endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"   #"http://127.0.0.1:8000"
 
 get_response = requests.get(endpoint,params={"abs":123}, json={"query":"hi bitch"})
 print(get_response.headers)
 
 #print(get_response.text)
-#print(get_response.json())
-#print(get_response.status_code)
 print(get_response.json())
+#print(get_response.status_code)
+#print(get_response.text)
 
