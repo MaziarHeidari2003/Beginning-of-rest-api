590b27c69dae1a4ecf155f4b460476baf931ea5e
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-05-08 13:15:01 -0700

    im tired , i dont know whats going on, its just code

--- DRF_3rd_tutorial/backend/api/views.py
+++ DRF_3rd_tutorial/backend/api/views.py
@@ -6,8 +6,62 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['POST'])
 def api_home(request,*args,**kwargs):
+
+  serializer = ProductSerializer(data=request.data)
+  if serializer.is_valid(raise_exception=True):      # new lesson raise exception
+    return Response(serializer.data)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #return Response({"Invalid":"Not good data"})
+
+
+
+  """
+
+
+  
+  date = request.data  # well i was temped to use request.POST
  
   instance = Product.objects.all().order_by("?").first()
   data = {}
@@ -18,7 +72,6 @@ def api_home(request,*args,**kwargs):
   return Response(data)
   
   #return HttpResponse(json_data_str,headers={"content-type": "application/json"})
-"""
  body = request.body   byte string of JSON data
    data = {}
    try:
