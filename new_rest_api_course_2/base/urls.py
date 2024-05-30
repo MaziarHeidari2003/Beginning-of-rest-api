b280313c43fe77e270d8719914a9a09d8d3b05ba
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-05-30 11:53:55 -0700

    practice makes perfect

--- new_rest_api_course_2/base/urls.py
+++ new_rest_api_course_2/base/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
   path('',endpoints),
-  path('advocates',advocate_list)
+  path('advocates',advocate_list),
+  path('advocates/<str:username>',advocate_detail)
 
 ]
